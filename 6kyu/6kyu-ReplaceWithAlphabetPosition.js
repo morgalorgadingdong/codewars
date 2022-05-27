a0ad00c2c1eba7ef7a651e9cf47e2f35e7094fdb
Morgan <morganfolz@gmail.com>
2022-05-27 12:33:15 -0700

    5.27.22 Training

---
+++ 6kyu/6kyu-ReplaceWithAlphabetPosition.js
@@ -0,0 +1,12 @@
+function alphabetPosition(text) {
+    let alph = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+    text=text.split('')
+    let output = ''
+    text.forEach((char,i) => {
+      let num = alph.indexOf(char.toUpperCase()) + 1
+      if (num > 0) {
+        output.length < 1 ? output = output.concat(num) : output = output.concat(' ', num)
+      }
+    })
+    return output;
+  }
