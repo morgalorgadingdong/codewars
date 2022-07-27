7a2e406299515d9ad26eabd0341d7aab1afa8977
Morgan <morganfolz@gmail.com>
2022-07-27 09:11:15 -0700

    7.27.22 Training

---
+++ 8kyu/8kyu-SwitchItUp.py
@@ -0,0 +1,22 @@
+def switch_it_up(number):
+    if number == 1:
+        number_str='One'
+    elif number == 2:
+        number_str='Two'
+    elif number == 3:
+        number_str='Three'
+    elif number == 4:
+        number_str='Four'
+    elif number == 5:
+        number_str='Five'
+    elif number == 6:
+        number_str='Six'
+    elif number == 7:
+        number_str='Seven'
+    elif number == 8:
+        number_str='Eight'
+    elif number == 9:
+        number_str='Nine'
+    else:
+        number_str = 'Zero'
+    return number_str
